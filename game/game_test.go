777812c4b1203c2da0040701f858bc61c87bbb87
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2024-08-24 07:13:40 -0600

    refactor: table tests for ListItems

--- game/game_test.go
+++ game/game_test.go
@@ -8,14 +8,24 @@ import (
 )
 func TestListItems_GivesCorrectResultForInput(t *testing.T){
     t.Parallel()
-    input := []string{
-        "a battery",
-        "a key",
-        "a tourist map",
+    type testCase struct {
+        input []string
+        want string
     }
-    want := "You can see here a battery, a key, and a tourist map."
-    got := game.ListItems(input)
-    if want != got {
-        t.Error(cmp.Diff(want, got))
+    cases := []testCase{
+        {
+        input: []string{
+            "a battery",
+            "a key",
+            "a tourist map",
+        },
+        want:  "You can see here a battery, a key, and a tourist map.",
+    },
+}
+    for _, tc := range cases {
+        got := game.ListItems(tc.input)
+        if tc.want != got {
+            t.Error(cmp.Diff(tc.want, got))
+        }
     }
 }
